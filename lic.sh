393dfd4ede319b83be01617fc355c1ca7bd0b669
Callum Walley <callum.walley@nesi.org.nz>
2020-09-29 22:22:19 +1300

    fixed malfunction in the auxiliary marker

--- lic.sh
+++ lic.sh
@@ -16,11 +16,18 @@ labaqus(){ echo "@$(awk '{print $2}' "$1")" ; }
 main(){
     found="Setting licence environment variables:\n"
     
-    get_lic "/opt/nesi/share/COMSOL/Licenses/*" "SINGULARITYENV_LMCOMSOL_LICENSE_FILE" lecho
-    get_lic "/opt/nesi/share/MATLAB/Licenses/*" "SINGULARITYENV_MLM_LICENSE_FILE" labaqus
-    get_lic "/opt/nesi/share/ANSYS/Licenses/*" "SINGULARITYENV_ANSYSLMD_LICENSE_FILE" lansys
-    get_lic "/opt/nesi/share/ANSYS/Licenses/*" "SINGULARITYENV_ANSOFTD_LICENSE_FILE" lansys
-    get_lic "/opt/nesi/share/ABAQUS/Licenses/*" "SINGULARITYENV_ABAQUSLM_LICENSE_FILE" lecho
+    # get_lic "/opt/nesi/share/COMSOL/Licenses/*" "SINGULARITYENV_LMCOMSOL_LICENSE_FILE" lecho
+    # get_lic "/opt/nesi/share/MATLAB/Licenses/*" "SINGULARITYENV_MLM_LICENSE_FILE" labaqus
+    # get_lic "/opt/nesi/share/ANSYS/Licenses/*" "SINGULARITYENV_ANSYSLMD_LICENSE_FILE" lansys
+    # get_lic "/opt/nesi/share/ANSYS/Licenses/*" "SINGULARITYENV_ANSOFTD_LICENSE_FILE" lansys
+    # get_lic "/opt/nesi/share/ABAQUS/Licenses/*" "SINGULARITYENV_ABAQUSLM_LICENSE_FILE" lecho
+    # printf '%b' "$found"
+
+    get_lic "/opt/nesi/share/COMSOL/Licenses/*" "LMCOMSOL_LICENSE_FILE" lecho
+    get_lic "/opt/nesi/share/MATLAB/Licenses/*" "MLM_LICENSE_FILE" labaqus
+    get_lic "/opt/nesi/share/ANSYS/Licenses/*" "ANSYSLMD_LICENSE_FILE" lansys
+    get_lic "/opt/nesi/share/ANSYS/Licenses/*" "ANSOFTD_LICENSE_FILE" lansys
+    get_lic "/opt/nesi/share/ABAQUS/Licenses/*" "ABAQUSLM_LICENSE_FILE" lecho
     printf '%b' "$found"
 }
 main
