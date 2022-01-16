5ca5344eb34b49a0f5d6948a3fc4e07407ece970
Callum Walley <callum.walley@nesi.org.nz>
2022-01-17 12:30:27 +1300

    Formatting

--- util/singularity_runscript.sh
+++ util/singularity_runscript.sh
@@ -1,29 +1,59 @@
 #!/bin/bash -e
-set -eo pipefail
+set -eu -o pipefail
 
-# Set paths
+################################################################################
+# Help                                                                         #
+################################################################################
+# Runscript, identical in purpose to %runscript inside container.
+# Starts VNC server pointing to noVNC. Port redirected with websockify.
+# Usage
+#   ./singularity_runscript.bash  socketport  localhost
+# Arguments
+#   socketport: Local port that Websockify will forward to.
+#   basepath:   Endpoint basepath. 'localhost' for testing.
+# Global:
+#   PATH
+#   CPATH
+#   LD_LIBRARY_PATH
+#       Paths to be appended.
+#   EBROOTCUDA = ""
+#       If set indicated CUDA loaded by LMOD, will add required CUDA paths.
+#   XDG_DATA_HOME = ${HOME}/.local/share
+#       Data files, following XDG specification.
+#   VDT_WEBSOCKOPTS = "-wm xfce4-session -autokill -securitytypes TLSNone,X509None,None"
+#       Additional options to pass to websockify.
+#       See https://linux.die.net/man/1/websockify 
+#   VDT_VNCOPTS = ""
+#       Additional options to pass to vncserver. 
+#       See https://linux.die.net/man/1/vncserver
+#######################################
+
+
+if [ -x "${XDG_CONFIG_HOME:=$HOME/.conf}/vdt/post.bash" ]; then
+    source "${XDG_CONFIG_HOME:=$HOME/.conf}/vdt/post.bash"
+fi
+
+# Append Paths
 export PATH="$PATH:/opt/slurm/bin:/opt/nesi/vdt/bin"
 export CPATH="$CPATH:/opt/slurm/include"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/slurm/lib64:/opt/slurm/lib64/slurm"
 
-if [ -x "${XDG_DATA_HOME:=$HOME/.conf}/vdt/post.bash" ]; then
- source "${XDG_DATA_HOME:=$HOME/.conf}/vdt/post.bash"
-fi
+# Set defaults for globals
+export VDT_WEBSOCKOPTS=${VDT_WEBSOCKOPTS:-"--web /opt/noVNC"}
+export VDT_VNCOPTS=${VDT_VNCOPTS:-"-wm xfce4-session -autokill -securitytypes TLSNone,X509None,None"}
 
 # CUDA specific.
-if [[ -n ${EBROOTCUDA} ]];then
+if [[ -n ${EBROOTCUDA} ]]; then
     export LD_LIBRARY_PATH="/cm/local/apps/cuda/libs/current/lib64:$EBROOTCUDA/lib64:$EBROOTCUDA/extras/CUPTI/lib64:$EBROOTCUDA/nvvm/lib64:$LD_LIBRARY_PATH"
     export PATH="$EBROOTCUDA:$EBROOTCUDA/nvvm/bin:$EBROOTCUDA/bin:/cm/local/apps/cuda/libs/current/bin:/cm/local/apps/cuda/libs/current/lib64:$PATH"
 fi
 
 # Find free port.
-VDT_DISPLAY_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')"
-/opt/websockify/run ${VDT_WEBSOCKOPTS} --web /opt/noVNC localhost:$VDT_SOCKET_PORT :$((VDT_DISPLAY_PORT+5900)) &
+vdt_display_port="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')"
 
-/opt/TurboVNC/bin/vncserver ${VDT_VNCOPTS} -wm xfce4-session -autokill -securitytypes TLSNone,X509None,None ":${VDT_DISPLAY_PORT}"    
+# Start websockify and vncserver.
+/opt/websockify/run ${VDT_WEBSOCKOPTS} localhost:$VDT_SOCKET_PORT :$((vdt_display_port + 5900)) &
+/opt/TurboVNC/bin/vncserver "${VDT_VNCOPTS} :${vdt_display_port}"
+# TODO: Proccess suicide pact
+
 wait
-#export XDG_DATA_DIRS="$XDG_DATA_DIRS:$EBROOTCUDA/share"
-
-
-
-
