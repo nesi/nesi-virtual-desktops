b54057321d529c857e4b704df88424c88b1800f5
Callum Walley <callum.walley@nesi.org.nz>
2022-02-13 14:08:35 +1300

    removed faulty bind paths, other things

--- jupyter_proxy_vdt/singularity_wrapper.bash
+++ jupyter_proxy_vdt/singularity_wrapper.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
-
+set -e -o pipefail
 
 usage(){
 	cat <<EOF
@@ -33,28 +32,32 @@ Config:
 EOF
 }
 
-if [ -x "${XDG_CONFIG_HOME:=$HOME/.conf}/vdt/pre.bash" ]; then
-    source "${XDG_CONFIG_HOME:=$HOME/.conf}/vdt/pre.bash"
+if [ -f "${XDG_CONFIG_HOME:=$HOME/.config}/vdt/pre.bash" ]; then
+    if [ ! -x "${XDG_CONFIG_HOME:=$HOME/.config}/vdt/pre.bash" ]; then
+        echo "'${XDG_CONFIG_HOME:=$HOME/.config}/vdt/pre.bash' has incorrect permissions. Fixing..."
+        chmod -v 760 "${XDG_CONFIG_HOME:=$HOME/.config}/vdt/pre.bash"
+    fi
+    source "${XDG_CONFIG_HOME:=$HOME/.config}/vdt/pre.bash"
 fi
 
 # Parse flags
 # TODO: Maybe have other paramters flaggable.
-params=""
-while (("$#")); do
-    case "$1" in
-    -h | --help)
-        usage && exit 0
-        shift
-        ;;
-    *)
-        params="$params $1"
-        shift
-        ;;
-    esac
-done
-eval set -- "$params"
+# params=""
+# while (("$#")); do
+#     case "$1" in
+#     -h | --help)
+#         usage && exit 0
+#         shift
+#         ;;
+#     *)
+#         params="$params $1"
+#         shift
+#         ;;
+#     esac
+# done
+# eval set -- "$params"
 
-if [[ $# -le 0 ]]; then
+if [[ $# -le 1 ]]; then
     echo "Not enough inputs." && usage && exit 1
 fi
 
@@ -66,19 +69,19 @@ fi
 # Load / unload required modules.
 module purge # > /dev/null  2>&1
 module unload XALT -q
-module load Python Singularity/3.8.5 -q
+module load Python Singularity/3.9.4 -q
 
 # Set default env variables.
 VDT_ROOT="${VDT_ROOT:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)"}"
-echo VDT_ROOT
-#VDT_BASE_IMAGE="${VDT_BASE_IMAGE:-"${VDT_ROOT}/sif"}"
-VDT_BASE_IMAGE="/opt/nesi/containers/vdt_base/dev_vdt_base.sif"
+VDT_BASE_IMAGE="${VDT_BASE_IMAGE:-"${VDT_ROOT}/sif"}"
+# THIS SHOULDNT BE NEEDED!!!
+#VDT_BASE_IMAGE="${VDT_BASE_IMAGE:-"/opt/nesi/containers/vdt_base/dev_vdt_base.sif"}"
 VDT_RUNSCRIPT="${VDT_RUNSCRIPT:-"${VDT_ROOT}/singularity_runscript.bash"}"
 VDT_OVERLAY="${VDT_OVERLAY:-"FALSE"}"
 VDT_GPU="${VDT_GPU:-"FALSE"}"
 
 # Overlay specific variables.
-VDT_OVERLAY_FILE="${VDT_OVERLAY_FILE:-"${XDG_DATA_HOME:=$HOME/.conf}/vdt/image_overlay"}"
+VDT_OVERLAY_FILE="${VDT_OVERLAY_FILE:-"${XDG_DATA_HOME:=$HOME/.config}/vdt/image_overlay"}"
 VDT_OVERLAY_COUNT="${VDT_OVERLAY_COUNT:-"10000"}"
 VDT_OVERLAY_BS="${VDT_OVERLAY_BS:-"1M"}"
 
@@ -86,7 +89,6 @@ LOGLEVEL="${LOGLEVEL:-"INFO"}"
 
 # Check validity of SIF
 # If pointing to directory, use sif in there.
-echo "$VDT_BASE_IMAGE"
 if [ -d ${VDT_BASE_IMAGE} ]; then
     echo "VDT_BASE_IMAGE is directory, looking for .sif"
     VDT_BASE_IMAGE="${VDT_BASE_IMAGE}/*.sif"
@@ -99,14 +101,15 @@ if [[ ! -x ${VDT_BASE_IMAGE} ]]; then
 fi
 
 # TODO: Should check if GPU avail first
-if [[ ${VDT_OVERLAY} == "TRUE" ]];then
+if [[ ${VDT_GPU} == "TRUE" ]];then
     module load CUDA
 fi
 
 # Bind minimal paths
-export SINGULARITY_BIND="${SINGULARITY_BIND:-\
+export SINGULARITY_BINDPATH="${SINGULARITY_BINDPATH:-\
 "/home,\
-/etc/hosts,
+/scale_wlg_persistent/filesets/project,\
+/etc/hosts,\
 /etc/opt/slurm,\
 /var/run/munge,\
 /opt/slurm,\
@@ -115,13 +118,14 @@ export SINGULARITY_BIND="${SINGULARITY_BIND:-\
 /scale_wlg_nobackup,\
 /nesi,\
 /cm,\
-/var/lib/sss/mc,\
 /opt/nesi,\
 /nesi/project,\
 /scale_wlg_persistent/filesets/project,\
 /nesi/nobackup,\
 /scale_wlg_nobackup/filesets/nobackup,\
 ${VDT_ROOT}"}"
+
+# /var/lib/sss/mc,\
 
 export SINGULARITYENV_LD_LIBRARY_PATH="${SINGULARITYENV_LD_LIBRARY_PATH:-$LD_LIBRARY_PATH}"
 export SINGULARITYENV_PATH=${SINGULARITYENV_PATH:-$PATH}
@@ -134,7 +138,7 @@ done
 
 # Create conf and data dirs.
 mkdir -vp   "${XDG_DATA_HOME:-$HOME/.local/share}/vdt" \
-            "${XDG_DATA_HOME:-$HOME/.conf}/vdt"
+            "${XDG_DATA_HOME:-$HOME/.config}/vdt"
 
 # Build command.
 if [[ $LOGLEVEL = "DEBUG" ]]; then
@@ -153,6 +157,14 @@ if [[ ${VDT_OVERLAY} == "TRUE" ]]; then
         rm -rf overlay_tmp"
     fi
     cmd="${cmd} --overlay ${VDT_OVERLAY_FILE}"
+    # If using overlay, replace index file.
+    echo "<meta http-equiv=\"refresh\" content=\"0; URL='${2}'\"/>" > "/opt/noVNC/index.html"
+else
+    # If not using overlay, need to mount redirect file.
+    temp_index_html=$(mktemp "${TMPDIR:-"/tmp"}/XXX")
+    echo "<meta http-equiv=\"refresh\" content=\"0; URL='${2}'\"/>" > "$temp_index_html"
+
+    export SINGULARITY_BINDPATH="$SINGULARITY_BINDPATH,${temp_index_html}:/opt/noVNC/index.html"
 fi
 
 cmd="${cmd} ${VDT_BASE_IMAGE} ${VDT_RUNSCRIPT} ${1}"
