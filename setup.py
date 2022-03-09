b2759ab5cc22e18cbe6ae46210377d8f224d835b
Callum Walley <callum.walley@nesi.org.nz>
2022-03-09 15:45:46 +1300

    formatting. incriment version

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools.command.install import install
 import os
 import subprocess
 
-module_out=subprocess.check_output("module -t avail | grep \"^R/\" | tail -n +2", stderr=subprocess.STDOUT, shell=True).decode("utf-8").strip()
+module_out = (
+    subprocess.check_output(
+        'module -t avail | grep "^R/" | tail -n +2',
+        stderr=subprocess.STDOUT,
+        shell=True,
+    )
+    .decode("utf-8")
+    .strip()
+)
+
 
 class InstallCommand(install):
-    user_options = install.user_options + [
-        ('setup=', None, "Additional setup steps")
-    ]
+    user_options = install.user_options + [("setup=", None, "Additional setup steps")]
 
     def initialize_options(self):
         install.initialize_options(self)
@@ -19,7 +26,8 @@ class InstallCommand(install):
 
     def run(self):
         if self.setup:
-            avail_versions=[]
+            avail_versions = []
+            avail_setup = []
             for file in os.listdir("setup_scripts"):
                 if file.endswith(".sh"):
                     avail_setup.append(file[:-3])
@@ -32,22 +40,30 @@ class InstallCommand(install):
                     print(f"'{setup_script}' is not a valid setup script.")
                     print(f"Valid options are {','.join(avail_setup)}")
         # self.setup
-
         install.run(self)
+
 
 setup(
     name="jupyter_proxy_vdt",
-    version="3.0.5",
+    version="3.0.6",
     description="launch vdt from jupyterproxy",
     url="https://github.com/nesi/nesi-virtual-desktops",
     packages=find_packages(),
     python_requires=">3.8",
-    package_data={"jupyter_proxy_vdt": ["crap_icon.svg", "singularity_wrapper.bash", "singularity_runscript.bash"]},
+    package_data={
+        "jupyter_proxy_vdt": [
+            "crap_icon.svg",
+            "singularity_wrapper.bash",
+            "singularity_runscript.bash",
+        ]
+    },
     entry_points={
-        "jupyter_serverproxy_servers": ["jupyter_proxy_vdt = jupyter_proxy_vdt:setup_jupyter_proxy_vdt"]
+        "jupyter_serverproxy_servers": [
+            "vdt = jupyter_proxy_vdt:setup_jupyter_proxy_vdt"
+        ]
     },
     cmdclass={
-        'install': InstallCommand,
+        "install": InstallCommand,
     },
     install_requires=["jupyter-server-proxy"],
 )
