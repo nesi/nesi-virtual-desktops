beb7fc3acdd2f1dbe2125f4a1d08f85a38eabcfe
Callum Walley <callum.walley@nesi.org.nz>
2022-03-09 15:46:14 +1300

    formatting

--- jupyter_proxy_vdt/__init__.py
+++ jupyter_proxy_vdt/__init__.py
@@ -5,25 +5,34 @@ import pkg_resources
 from pkg_resources import resource_filename
 from pathlib import Path
 
+
 def setup_jupyter_proxy_vdt():
 
     pkg_path = "jupyter_proxy_vdt"
     icon_path = resource_filename(pkg_path, "crap_icon.svg")
     wrapper_path = resource_filename(pkg_path, "singularity_wrapper.bash")
-    runscript_path = resource_filename(pkg_path, "singularity_runscript.bash") # Is inferred in wrapper.
+    runscript_path = resource_filename(
+        pkg_path, "singularity_runscript.bash"
+    )  # Is inferred in wrapper.
 
     pkg_version = pkg_resources.require(pkg_path)[0].version
     launcher_title = f"Virtual Desktop {pkg_version}"
-    
+
     return {
-    "command": [wrapper_path, "{port}", "vnc.html?path={base_url}vdt/vnc.html&autoconnect=true&resize=remote"],
-    "timeout": 300,
-    "environment": {"VDT_BASE_IMAGE": "/opt/nesi/containers/vdt_base/dev_vdt_base.sif"},
-    "absolute_url": False,
-    "new_browser_tab":True,
+        "command": [
+            wrapper_path,
+            "{port}",
+            "vnc.html?path={base_url}vdt/vnc.html&autoconnect=true&resize=remote",
+        ],
+        "timeout": 300,
+        "environment": {
+            "VDT_BASE_IMAGE": "/opt/nesi/containers/vdt_base/dev_vdt_base.sif"
+        },
+        "absolute_url": False,
+        "new_browser_tab": True,
         "launcher_entry": {
             "icon_path": icon_path,
             "title": launcher_title,
-            "enabled": True
+            "enabled": True,
         },
     }
